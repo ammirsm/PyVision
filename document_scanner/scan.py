a3677c325eca11e7275823ef24c40e392f26ee7c
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2020-01-03 15:46:36 +0330

    transform added to document scanner

--- document_scanner/scan.py
+++ document_scanner/scan.py
@@ -33,19 +33,25 @@ cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 for c in cnts:
     #approximate the counter
     peri = cv2.arcLength(c, True)
-    print(peri)
     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    print(approx)
-    cv2.drawContours(image, [approx], -1, (0, 0, 255), 2)
 
     if len(approx) == 4:
         screenCnt = approx
         # break
 
 print("STEP 2: Find counters of paper")
-# cv2.drawContours(image, [screenCnt], -1, (0,255,0), 2)
+cv2.drawContours(image, [screenCnt], -1, (0,255,0), 2)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+warped = four_point_transform(orig, screenCnt.reshape(4,2) * ratio)
 
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset=10, method="gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+print("STEP 3")
+cv2.imshow("original", imutils.resize(orig, height=650))
+cv2.imshow("Scanned", imutils.resize(warped, height=650))
+cv2.waitKey(0)
